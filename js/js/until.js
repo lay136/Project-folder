1037e57fd924da01afd44a617624f7dca05fb495
小辉 <1652016835@qq.com>
2019-12-04 17:24:10 +0800

    12.4 p.m.

--- js/js/until.js
+++ js/js/until.js
@@ -3,37 +3,42 @@
 	attr 要改变的是那个属性
 	target 要将该属性的值改到多少
 */
-function animate(obj,attr,target){
-	clearInterval(obj.timer);
+//匀速动画
+function animate(obj,attr,target){//animate函数里面写参数
+	clearInterval(obj.timer);//关闭定时器
+	var iSpeed = 0;
 	obj.timer = setInterval(function(){
-		var iSpeed = 0;
 		var current = parseFloat(getComputedStyle(obj,false)[attr]);
+		//getComputedStyle获取所有属性的值
+
 		if(attr == 'opacity'){
-			current = Math.round(current * 100);
+			current = Math.round(current * 100);//针对透明度的设置
 		}
+		//匀速动画速度
 		if(current < target){
-			iSpeed = 8;
+			iSpeed = 9;
 		}else{
-			iSpeed = -8;
+			iSpeed = -9;
 		}
-		if( Math.abs(target - current) < Math.abs(iSpeed)){
+		//匀速动画的结束条件
+		if(Math.abs(target - current) < Math.abs(iSpeed)){
 			if(attr == 'opacity'){
-				obj.style[attr] = target/100;
+			//判断传进的值，如果是opacity除以100，否则不除，加长度单位
+				obj.style[attr] = target / 100;
 			}else{
 				obj.style[attr] = target + 'px';
 			}
-			clearInterval(obj.timer);
+			clearInterval(obj.timer);//关闭定时器
 		}else{
-			if(attr == 'opacity'){
-				obj.style[attr] = (current + iSpeed)/100;
+			if(attr == 'opacity'){//改变样式
+				obj.style[attr] = (current + iSpeed) / 100;
 			}else{
 				obj.style[attr] = current + iSpeed + 'px';
 			}
-			
 		}
 	},30);
 }
 
-function getScrollTop(){
+function getScrollTop(){//window渲染模式处理函数
 	return window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;
 }
